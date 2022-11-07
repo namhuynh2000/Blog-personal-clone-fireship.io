74416c852fed1a2f7d87c0d368064d7447c93e32
Ruins <hqnam.it@gmail.com>
2022-11-07 21:22:46 +0700

    build again

--- components/Layouts/components/Footer.js
+++ components/Layouts/components/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import CrossBar from '../../CrossBar'
 
@@ -6,7 +7,7 @@ export default function Footer() {
         <>
             <CrossBar />
             <div className="inner text-[#b2becd] flex flex-col items-center gap-3 p-[32px] mb-[24px]">
-                <p>Find an issue with this page? <a target='_blank' className='text-[#3b82f6]' href="https://github.com/namhuynh2000/Blog-personal-clone-fireship.io.git">Fix it on GitHub</a></p>
+                <p>Find an issue with this page? <Link className='text-[#3b82f6]' href="https://github.com/namhuynh2000/Blog-personal-clone-fireship.io.git">Fix it on GitHub</Link></p>
                 <p>Need help? Email <a href="mailto:mooncomvn130@gmail.com"><strong>mooncomvn130@gmail</strong></a></p>
                 <small>Copyright © 2022 Fireship LLC</small>
             </div>
