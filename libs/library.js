838777fdbf5d1a0625caed4322e34b44f03c378e
Ruins <hqnam.it@gmail.com>
2022-11-08 18:24:42 +0700

    postPerPage=5

--- libs/library.js
+++ libs/library.js
@@ -3,7 +3,7 @@ import { indexAlgolia } from '../utils/Algolia';
 import { clientSanity } from '../utils/Sanity';
 
 // Variables
-export const postPerPage = 20;
+export const postPerPage = 5;
 const builder = imageUrlBuilder(clientSanity);
 
 // Function
